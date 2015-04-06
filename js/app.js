0e46b7bcce733d2fe3ce2615747b53b3f51e0fc1
Johannes Scharlach <johannes.scharlach@gmail.com>
2015-04-06 12:04:44 +0200

    multiple cats

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ var Cat = function(data) {
         max: Infinity,
         level: 'Adult'
       }
-    ];
+    ],
+    self = this;
 
     this.clickCount = ko.observable(data.clickCount);
     this.imgAttribution = ko.observable(data.imgAttribution);
@@ -32,19 +33,65 @@ var Cat = function(data) {
     this.name = ko.observable(data.name);
 };
 
-
-var ViewModel = function() {
-  this.currentCat = ko.observable(new Cat({
+var initialCats = [{
     clickCount: 0,
     imgAttribution: '',
     imgSrc: 'img/22252709_010df3379e_z.jpg',
     levelId: 0,
     nickNames: ['Jani', 'Miss J.', 'Little JJ'],
     name: 'Jana'
-  }));
+  },
+  {
+    clickCount: 0,
+    imgAttribution: '',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    levelId: 0,
+    nickNames: ['Jani', 'Miss J.', 'Little JJ'],
+    name: 'Theresa'
+  },
+  {
+    clickCount: 0,
+    imgAttribution: '',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    levelId: 0,
+    nickNames: ['Franzi'],
+    name: 'Francesca'
+  },
+  {
+    clickCount: 0,
+    imgAttribution: '',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    levelId: 0,
+    nickNames: ['July'],
+    name: 'Julia'
+  },
+  {
+    clickCount: 0,
+    imgAttribution: '',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    levelId: 0,
+    nickNames: ['Mary'],
+    name: 'Maria'
+  },
+];
+
+var ViewModel = function() {
+  var self = this;
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
+  };
+
+  this.switchCats = function() {
+    self.currentCat(this);
   };
 };
 
